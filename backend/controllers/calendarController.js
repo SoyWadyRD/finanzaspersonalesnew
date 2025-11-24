6382a323716190a0a4608fda58bfdb7ead5ec13a
SoyWadyRD <andreslopez261316@gmail.com>
2025-11-23 23:53:44 -0400

    Calendario arreglado fecha

--- backend/controllers/calendarController.js
+++ backend/controllers/calendarController.js
@@ -34,7 +34,17 @@ exports.obtenerCalendarios = async (req, res) => {
     }
 
     const fechaRegistro = moment(usuario.createdAt); // Fecha de registro del usuario
-    const fechaHoy = moment().startOf('day'); // Aseguramos que la fecha de hoy esté sin horas/minutos
+    const fechaHoy = moment().hours(12).minutes(0).seconds(0);  // Hora establecida a las 12:00 PM
+if (fechaHoy.isAfter(moment(usuario.createdAt))) {
+  // Si estamos después de las 12 PM y el usuario ya está registrado, agrega un nuevo día
+  const nuevoDia = {
+    dia: fechaHoy.date(),
+    fecha: fechaHoy.format('YYYY-MM-DD')
+  };
+  mesHoy.dias.push(nuevoDia);
+}
+
+
 
     // Crear un array para los meses
     const meses = [];
